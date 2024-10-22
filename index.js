9f0d7f1abb83bc4443cd115e26dcb6a01b8802e7
Rajeshwari-Pawar <rajeshwaripawar2308@gmail.com>
2024-10-22 19:53:20 +0530

    created another function to forecast the future weather data for 5 days which is being passed in the previous function of findingCityCoordinates

--- index.js
+++ index.js
@@ -5,6 +5,13 @@ const apiKey = "13d472083a14db983ea3e246a52880e3";
 
 searchWeatherBtn.addEventListener("click", findCityCoordinates);
 
+
+
+
+
+
+
+
 async function findCityCoordinates() {
     const inputCity = inputField.value.trim();
     if (!inputCity) return;
@@ -24,5 +31,46 @@ async function findCityCoordinates() {
     } catch (error) {
         console.error(error);
         alert("Failed to fetch coordinates. Please try again.");
+    }
+};
+
+
+
+
+
+async function getForecast (cityName, latitude, longitude) {
+    
+    const weatherAPI = `http://api.openweathermap.org/data/2.5/forecast?lat=${latitude}&lon=${longitude}&units=metric&appid=${apiKey}`;
+
+    try {
+        const response = await fetch(weatherAPI);
+
+        const forecastData = await response.json();
+        
+
+        const days = [];
+
+        const filteredData = forecastData.list.filter(forecast => {
+            const date = new Date(forecast.dt_txt).getDate();
+            if (!days.includes(date)) {
+                return days.push(date);
+            }
+        });
+
+        console.log(filteredData);
+
+        /*inputField.value = "";
+        weatherDisplayTop.innerHTML = "";
+        forecastDisplayBottom.innerHTML = "";*/
+
+        filteredData.forEach((forecastItem, idx) => {
+            /*if (idx === 0) {
+                weatherDisplayTop.insertAdjacentHTML("beforeend", generateWeatherCard(cityName, forecastItem, idx));
+            } else {
+                forecastDisplayBottom.insertAdjacentHTML("beforeend", generateWeatherCard(cityName, forecastItem, idx));
+            }*/
+        });
+    } catch (error) {
+        alert("Error fetching weather data!");
     }
 };
