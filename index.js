4a93dbe2fcd476fd5bc3fb0a831f1401f6737fe9
Rajeshwari-Pawar <rajeshwaripawar2308@gmail.com>
2024-10-22 11:43:06 +0530

    added eventlistener to search btn and created a function to find weather data using city name and its latitude and longitude

--- index.js
+++ index.js
@@ -1,2 +1,28 @@
 const inputField = document.querySelector(".inputField");
 const searchWeatherBtn = document.querySelector(".searchBtn");
+
+const apiKey = "13d472083a14db983ea3e246a52880e3";
+
+searchWeatherBtn.addEventListener("click", findCityCoordinates);
+
+async function findCityCoordinates() {
+    const inputCity = inputField.value.trim();
+    if (!inputCity) return;
+
+    const geoAPI = `http://api.openweathermap.org/geo/1.0/direct?q=${inputCity}&limit=1&appid=${apiKey}`;
+
+    try {
+        const response = await fetch(geoAPI);
+        const locationData = await response.json();
+
+        if (!locationData.length) {
+            alert(`No coordinates found for ${inputCity}`);
+        } else {
+            const { name, lat, lon } = locationData[0];
+            getForecast(name, lat, lon);
+        }
+    } catch (error) {
+        console.error(error);
+        alert("Failed to fetch coordinates. Please try again.");
+    }
+};
